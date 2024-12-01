d0d5655057b19de2e77a65b018796bd535025f68
Cristhian <cristhian.atuncar@unmsm.edu.pe>
2024-12-01 16:46:59 -0500

    Adding functional tests

---
+++ client/src/__tests__/ProductCard.test.js
@@ -0,0 +1,39 @@
+const { Builder, By, until, Browser } = require('selenium-webdriver');
+const { strictEqual } = require('assert');
+
+describe('ProductCard', () => {
+  let driver;
+
+  jest.setTimeout(15000);
+
+  beforeAll(async () => {
+    driver = await new Builder().forBrowser(Browser.CHROME).build();
+    await driver.get('http://localhost:3000');
+  });
+
+  afterAll(async () => {
+    await driver.quit();
+  });
+
+  const esperarLoader = async (selector, timeout = 10000) => {
+    const elemento = await driver.wait(until.elementLocated(By.css(selector)), timeout);
+    await driver.wait(until.elementIsVisible(elemento), timeout);
+    return elemento;
+  };
+
+  it('Aviso de no puede añadir favorito', async () => {
+    try {
+      const favoriteCheckbox = await esperarLoader('input[type="checkbox"]');
+
+      if (await favoriteCheckbox.isEnabled()) {
+        await favoriteCheckbox.click();
+      }
+
+      const notification = await esperarLoader('.notification-favorites', 5000);
+      const notificationText = await notification.getText();
+      strictEqual(notificationText, 'Inicia sesión para añadir a favoritos');
+    } catch (error) {
+      console.log('Error al interactuar con el checkbox o la notificación:', error);
+    }
+  });
+});
---
+++ client/src/__tests__/ProductDescription.test.js
@@ -0,0 +1,43 @@
+const { Builder, By, until, Browser } = require('selenium-webdriver');
+const { strictEqual, ok } = require('assert');
+
+describe('ProductDescription', () => {
+  let driver;
+
+  jest.setTimeout(15000);
+
+  beforeAll(async () => {
+    driver = await new Builder().forBrowser(Browser.CHROME).build();
+    await driver.get('http://localhost:3000/product/19')//Producto de ejemplo
+  });
+
+  afterAll(async () => {
+    await driver.quit();
+  });
+
+  const esperarLoader = async (selector, timeout = 10000) => {
+    const elemento = await driver.wait(until.elementLocated(By.css(selector)), timeout);
+    await driver.wait(until.elementIsVisible(elemento), timeout);
+    return elemento;
+  };
+
+  it('Verifica la notificación después de añadir al carrito', async () => {
+    try {
+      await esperarLoader('.background-loader');
+
+      const addToCartButton = await driver.wait(until.elementLocated(By.css('.add-to-car')), 5000);
+
+      await addToCartButton.click();
+
+      const notification = await esperarLoader('.notification-cart', 5000);
+      const notificationText = await notification.getText();
+      strictEqual(notificationText, 'Producto añadido al carrito');
+
+      const checkmark = await driver.findElement(By.css('.notification-cart .FaCircleCheck'));
+      ok(checkmark, 'El ícono de checkmark debería aparecer en la notificación');
+
+    } catch (error) {
+      console.log('Error al interactuar con el botón de agregar al carrito o la notificación:', error);
+    }
+  });
+});
